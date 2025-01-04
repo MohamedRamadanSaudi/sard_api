22205c4c1fbfcf160b06e9a59846ba7892c9ba3a
Mohamed Ramadan <91095064+MohamedRamadanSaudi@users.noreply.github.com>
2025-01-04 17:43:12 +0200

    feat: author functionality

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthorsService } from './authors.service';
 import { AuthorsController } from './authors.controller';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [AuthorsController],
-  providers: [AuthorsService],
+  providers: [AuthorsService, PrismaService, JwtService, CloudinaryService],
 })
-export class AuthorsModule {}
+export class AuthorsModule { }
--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -1,26 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
+import { parse } from 'path';
 
 @Injectable()
 export class AuthorsService {
-  create(createAuthorDto: CreateAuthorDto) {
-    return 'This action adds a new author';
+  constructor(private readonly prisma: PrismaService,
+    private readonly cloudinaryService: CloudinaryService,
+  ) { }
+
+  async create(createAuthorDto: CreateAuthorDto, photo?: Express.Multer.File) {
+    let photoUrl = '';
+    if (photo) {
+      photoUrl = await this.cloudinaryService.uploadImage(photo);
+    }
+    const author = await this.prisma.author.create({
+      data: {
+        ...createAuthorDto,
+        photo: photoUrl,
+      },
+    });
+    return author;
   }
 
   findAll() {
-    return `This action returns all authors`;
+    return this.prisma.author.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} author`;
+  findOne(id: string) {
+    return this.prisma.author.findUnique({ where: { id } });
   }
 
-  update(id: number, updateAuthorDto: UpdateAuthorDto) {
-    return `This action updates a #${id} author`;
+  async update(id: string, updateAuthorDto: UpdateAuthorDto, photo?: Express.Multer.File) {
+    const author = await this.prisma.author.findUnique({
+      where: { id },
+      select: {
+        photo: true,
+      },
+    });
+    if (!author) {
+      throw new NotFoundException('Author not found');
+    }
+
+    let photoUrl = '';
+    if (photo) {
+      photoUrl = await this.cloudinaryService.uploadImage(photo);
+    }
+
+    if (author?.photo) {
+      const urlParts = author.photo.split('/'); // Split the URL
+      const publicIdWithExtension = urlParts[urlParts.length - 1]; // Get the last part
+      const publicId = parse(publicIdWithExtension).name; // Remove the file extension
+
+      await this.cloudinaryService.deleteImage(`sard_uploads/${publicId}`); // Adjust the folder path if necessary
+    }
+
+    const result = await this.prisma.author.update({
+      where: { id },
+      data: { ...updateAuthorDto, photo: photoUrl },
+    });
+    return result;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} author`;
+  async remove(id: string) {
+    const author = await this.prisma.author.findFirst({ where: { id } });
+    if (!author) {
+      throw new NotFoundException('Author not found');
+    }
+    if (author?.photo) {
+      const urlParts = author.photo.split('/'); // Split the URL
+      const publicIdWithExtension = urlParts[urlParts.length - 1]; // Get the last part
+      const publicId = parse(publicIdWithExtension).name; // Remove the file extension
+
+      await this.cloudinaryService.deleteImage(`sard_uploads/${publicId}`); // Adjust the folder path if necessary
+    }
+    await this.prisma.author.delete({ where: { id } });
+    return { message: 'Author deleted successfully' };
   }
 }
