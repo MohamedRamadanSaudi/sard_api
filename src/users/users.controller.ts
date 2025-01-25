5f1ad50e38b5520c29de860abeb8da060fcc656e
Mohamed Ramadan <91095064+MohamedRamadanSaudi@users.noreply.github.com>
2025-01-25 17:41:57 +0200

    feat: get me

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
@@ -24,6 +24,13 @@ export class UsersController {
   @Roles('admin')
   getUsers() {
     return this.usersService.getUsers();
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'user')
+  getMe(@Req() req) {
+    return this.usersService.getUser(req.user.userId);
   }
 
   @Get(":id")
